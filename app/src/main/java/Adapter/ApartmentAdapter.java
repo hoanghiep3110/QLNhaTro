f1b3f38265564ccd5a28e421cd1f1c03263a2b13
hai2606 <86307572+hai0626@users.noreply.github.com>
2021-10-11 16:16:57 +0700

    update 2

---
+++ app/src/main/java/Adapter/ApartmentAdapter.java
@@ -0,0 +1,70 @@
+package Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.qlnhatro.R;
+
+import java.util.ArrayList;
+
+import House.Apartment;
+
+public class ApartmentAdapter extends RecyclerView.Adapter<ApartmentAdapter.ViewHolder> {
+
+    private ArrayList<Apartment> alApartmen;
+
+    public ApartmentAdapter(ArrayList<Apartment> alApartment, Context context) {
+        this.alApartmen = alApartmen;
+        this.context = context;
+    }
+
+    private Context context;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R  .layout.apartment_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.txtTenkhach.setText(alApartmen.get(position).getTenKhach());
+        holder.txtTenphong.setText(alApartmen.get(position).getTenPhong());
+        holder.btnDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context,"Đang chọn nút" + (holder.getAdapterPosition()+1),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return alApartmen.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView txtTenphong,txtTenkhach;
+        public Button btnDetails;
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            txtTenphong = itemView.findViewById(R.id.txtTenphong);
+            txtTenkhach = itemView.findViewById(R.id.txtTenkhach);
+            btnDetails = itemView.findViewById(R.id.btnDetails);
+        }
+    }
+}
--- app/src/main/java/com/example/qlnhatro/MainActivity4.java
+++ app/src/main/java/com/example/qlnhatro/MainActivity4.java
@@ -1,14 +1,46 @@
 package com.example.qlnhatro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+import Adapter.ApartmentAdapter;
+import House.Apartment;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    private Button btnChange;
+    private ArrayList<Apartment> alApartment;
+
+    private RecyclerView rclApartmentList;
+    private ApartmentAdapter apartmentAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        rclApartmentList = findViewById(R.id.rclApartmentList);
+
+        alApartment = new ArrayList<>();
+        for (int i = 1; i < 50; i++) {
+            alApartment.add(new Apartment("Ten Phong " +i,"Ten Khach"+i));
+        }
+
+        rclApartmentList.setHasFixedSize(true);
+
+        apartmentAdapter = new ApartmentAdapter(alApartment,this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+
+        rclApartmentList.setLayoutManager(linearLayoutManager);
+        rclApartmentList.setAdapter(apartmentAdapter);
     }
 }
