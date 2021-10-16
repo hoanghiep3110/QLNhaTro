42f96baf8737619e36166a0f65a4c1dc34a8d708
hai2606 <86307572+hai0626@users.noreply.github.com>
2021-10-16 17:26:18 +0700

    Register

--- app/src/main/java/com/example/qlnhatro/MainActivity2.java
+++ app/src/main/java/com/example/qlnhatro/MainActivity2.java
@@ -32,40 +32,40 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-//        edtTen = findViewById(R.id.edtTen);
-//        edtDc = findViewById(R.id.edtDc);
-//        edtSdt = findViewById(R.id.edtSdt);
-//        edtTK = findViewById(R.id.edtTK);
-//        edtMk = findViewById(R.id.edtMk);
-//        btnDk = findViewById(R.id.btnDk);
-//        db = new Account(this);
-//
-//        btnDk.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String name=edtTen.getText().toString().trim();
-//                String phone= edtSdt.getText().toString().trim();
-//                String address=edtDc.getText().toString().trim();
-//                String username=edtTK.getText().toString().trim();
-//                String password=edtMk.getText().toString().trim();
-//                Accounts taikhoan = create();
-//
-//
-//                if(name.equals("")||address.equals("")
-//                ||phone.equals("")||username.equals("")
-//                ||password.equals("")){
-//                    Toast.makeText(MainActivity2.this, "Không được bỏ trống thông tin", Toast.LENGTH_SHORT).show();
-//                }
-//                else {
-//                    db.insertAccount(taikhoan);
-//                    Intent intent = new Intent(MainActivity2.this,MainActivity.class);
-//                    startActivity(intent);
-//                    Toast.makeText(MainActivity2.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
-//                }
-//
-//
-//            }
-//        });
+        edtTen = findViewById(R.id.edtTen);
+        edtDc = findViewById(R.id.edtDc);
+        edtSdt = findViewById(R.id.edtSdt);
+        edtTK = findViewById(R.id.edtTk);
+        edtMk = findViewById(R.id.edtMk);
+        btnDk = findViewById(R.id.btnDk);
+        db = new Account(this);
+
+        btnDk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=edtTen.getText().toString().trim();
+                String phone= edtSdt.getText().toString().trim();
+                String address=edtDc.getText().toString().trim();
+                String username=edtTK.getText().toString().trim();
+                String password=edtMk.getText().toString().trim();
+                Accounts taikhoan = create();
+
+
+                if(name.equals("")||address.equals("")
+                ||phone.equals("")||username.equals("")
+                ||password.equals("")){
+                    Toast.makeText(MainActivity2.this, "Không được bỏ trống thông tin", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    db.insertAccount(taikhoan);
+                    Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity2.this, "Đăng kí thành công", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
     }
 
     public Accounts create(){
